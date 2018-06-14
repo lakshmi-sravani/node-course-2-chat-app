3668e67885863d4c2b2049b36849ca4e9eb8bc13
wtt161 <wtt161@localhost.localdomain>
2018-06-14 18:44:24 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public')
 
 var app = express();
@@ -14,13 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('new user connected');
 
+    //socket.emit from Admin text welcome to the chat app
+    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+
+    // socket.broadcast.emit from admin text how user joined
+     socket.broadcast.emit('newMessage',generateMessage('Admin', 'new user joined'));
+
+
     socket.on('createMessage', (message) => {
         console.log('createMessage',message);
-        io.emit('newMessage',{
-            from:message.from,
-            text:message.text,
-            createAt: new Date().getTime()
-        })
+        io.emit('newMessage',generateMessage(message.from, message.text));
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () =>{
---
+++ server/utils/message.test.js
@@ -0,0 +1,20 @@
+var expect = require('expect');
+
+var{generateMessage} = require('./message');
+
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+       var from = 'jen';
+       var text = 'some message';
+       var message = generateMessage(from,text);
+
+       expect(message.createdAt).toBeA('number');
+       expect(message).toInclude({from,text})
+       
+        //store res in variable
+        //assert from match 
+        //assrert text match
+        //assert createdAt is number
+    });
+});
