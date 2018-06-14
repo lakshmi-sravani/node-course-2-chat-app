f4ac83a56a421541bf1c875f061832467e39cd1e
wtt161 <wtt161@localhost.localdomain>
2018-06-14 13:07:03 +0530

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
+ 
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public')
 // console.log(__dirname + '/../public');
 // console.log();
 var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
+
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) =>{
+console.log('new user connected');
+
+socket.on('disconnect', () =>{
+            console.log('disconnected from server');
+        });
+});
+
+server.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
