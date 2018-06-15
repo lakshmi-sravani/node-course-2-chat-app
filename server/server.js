e70b5117de9393117b97f0afd4bf2adece631919
wtt161 <wtt161@localhost.localdomain>
2018-06-15 16:06:35 +0530

    Add form for messages and show income messages

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
- const path = require('path');
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
@@ -22,9 +22,10 @@ io.on('connection', (socket) =>{
      socket.broadcast.emit('newMessage',generateMessage('Admin', 'new user joined'));
 
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage',message);
         io.emit('newMessage',generateMessage(message.from, message.text));
+        callback('from the server');
         // socket.broadcast.emit('newMessage',{
         //     from:message.from,
         //     text:message.text,
